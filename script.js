e336746570247a9cfa6cd9c1854bf09fb2a90327
Rayhanul Islam Riaj <riaj2517@gmail.com>
2024-02-04 00:41:11 +0600

    first commit

--- script.js
+++ script.js
@@ -5,16 +5,49 @@ let messageError = document.getElementById('message-error');
 let submitError = document.getElementById('submit-error');
 
 function validateName() {
-    var name = document.getElementById('contact-name');
+    var name = document.getElementById('contact-name').value;
 
     if(name.length == 0) {
-        nameError.innerHTML = 'Name is required';
+        nameError.innerHTML = 'Name is required!';
         return false;
     }
-    if(!name.matche(/^[A-Za-Z]*\s{1}[A-Za-z]*$/)) {
-        nameError.innerHTML = 'Write full name';
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = 'Write full name!';
         return false;
     }
-    nameError.innerHTML = 'valied';
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+function validatePhone() {
+    var phone = document.getElementById('contact-phone').value;
+
+    if(phone.length == 0) {
+        phoneError.innerHTML = 'Phone number is required';
+        return false;
+    }
+    if(phone.length !== 11) {
+        phoneError.innerHTML = 'Phone number should be 11 digits!';
+        return false;
+    }
+    if(!phone.match(/^[0-9]{11}$/)) {
+        phoneError.innerHTML = 'Only digits please!';
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateEmail() {
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0) {
+        emailError.innerHTML = 'Email is required!';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Email Invalid!';
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
     return true;
 }
